aa76af869bf9eac488d5186e16ca920bc137ebaa
7657137 <7657137@student.swin.edu.au>
2017-05-22 19:21:50 +1000

    Added argument inputs

--- PropLogic/Program.cs
+++ PropLogic/Program.cs
@@ -6,10 +6,30 @@ namespace PropLogic
 	{
 		public static void Main (string[] args)
 		{
-			LogicEngine Thinker = new LogicEngine( new KnowledgeBase ("test1.txt"));
-			//Thinker.ForwardChain ();
-			//Thinker.BackChain ();
-			Thinker.TruthTableMethod();
+			LogicEngine Thinker;
+			if (args.Length > 1) {
+				try {
+					Thinker = new LogicEngine (new KnowledgeBase (args [1]));
+				} catch {
+					Thinker = new LogicEngine (new KnowledgeBase ("test1.txt"));
+				}
+
+				switch (args [0].ToLower ()) {
+				case "tt":
+					Thinker.TruthTableMethod ();
+					break;
+				case "fc":
+					Thinker.ForwardChain ();
+					break;
+				case "bc":
+					Thinker.BackChain ();
+					break;
+				default:
+					Thinker.TruthTableMethod ();
+					break;
+				}
+			} else
+				Console.WriteLine ("Insufficient arguments");
 		}
 	}
 }
